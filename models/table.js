0d454723c7603fc0218a8ceaf2b292ef58f2e478
GUNDAANUTEJ <77635770+GUNDAANUTEJ@users.noreply.github.com>
2021-04-07 22:47:57 -0500

    add table

---
+++ models/table.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+const tableSchema = mongoose.Schema({
+  Brand: String,
+quality: String,
+cost: Number
+})
+module.exports = mongoose.model("table", tableSchema)
