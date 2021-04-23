baa5d0ff2fff362d9ea2d8aafe8620d1f3b040ce
GUNDAANUTEJ <77635770+GUNDAANUTEJ@users.noreply.github.com>
2021-04-23 14:18:48 -0500

    protected update endpoint

--- routes/table.js
+++ routes/table.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const table_controlers= require('../controllers/table');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
 
 /* GET tables */
 router.get('/', table_controlers.table_view_all_Page );
@@ -13,7 +23,7 @@ router.get('/detail', table_controlers.table_view_one_Page);
 router.get('/create', table_controlers.table_create_Page);
 
 /* GET create update page */
-router.get('/update', table_controlers.table_update_Page);
+router.get('/update', secured, table_controlers.table_update_Page);
 
 /* GET create Icecream page */
 router.get('/delete', table_controlers.table_delete_Page);
